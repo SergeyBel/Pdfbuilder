9e67bd7031646e7047d8468273974b9d9d29f61d
SergeyBel <belovseregas@gmail.com>
2018-12-03 22:15:03 +0300

    fix example1

--- Examples/example1.php
+++ Examples/example1.php
@@ -2,7 +2,6 @@
 
 namespace PdfBuilder;
 
-use PdfBuilder\Font\Courier;
 use PdfBuilder\Format\A4;
 
 require_once __DIR__.'/../vendor/autoload.php';
@@ -11,7 +10,6 @@ require_once __DIR__.'/../vendor/autoload.php';
 $builder = new PdfBuilder(new A4());
 
 $pdf = $builder
-  ->setFont(new Courier(12))
   ->write('Hello World!')
   ->build();
 $pdf->saveToFile('example1.pdf');
